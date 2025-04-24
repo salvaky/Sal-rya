82bb47bffbc63e8dd6c11673a86fc514e4c8efeb
salvaky <salvakeylarindiani@gmail.com>
2025-04-24 13:43:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.image("view/20250318_141545_602.webp")
