214b322f463879e4bdd534507614174928adcdb4
salvaky <salvakeylarindiani@gmail.com>
2025-05-15 14:29:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.st.title("🎈 aku suka kamu")
+st.write(sal)
+st.image("Views/IMG_20250508_101454_265.webp", witdh=200)  
+st.write("\n")
+st.subheader("Salva Keyla R")
+st.image(
+    "Mari latihan membuat website melalui Github dan Streamlit"
 )
-
-st.image("views/20250318_141545_602.webp")
